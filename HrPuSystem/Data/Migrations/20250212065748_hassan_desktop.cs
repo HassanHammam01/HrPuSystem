d0f5ff3065cf0436dcf2d106bda19604cda0955a
Hassan.Hammam <Hassan.Hammam@IT-SUPPORT-01>
2025-02-12 09:52:02 +0200

    Employee

---
+++ HrPuSystem/Data/Migrations/20250212065748_hassan_desktop.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HrPuSystem.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class hassan_desktop : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
